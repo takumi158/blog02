d603c95d9e5295cedf16de4153e133af39005d5c
齋藤　薫実 <takumi158@saitoukaorujitsunoMacBook-puro.local>
2018-05-14 13:45:11 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'top/index'
   resources :bbs
 
-  root "bbs#index"
+  root "top#index"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
